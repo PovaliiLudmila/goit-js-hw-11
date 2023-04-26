a4c1827d431d2c42530622bc83bd105c7a0d1c93
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-04-26 20:04:49 +0300

    12345

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -1,5 +1,3 @@
-import { getRefs } from './getRefs';
-
 export function getRefs() {
     return {
       form: document.querySelector('#search-form'),
@@ -9,14 +7,4 @@ export function getRefs() {
       body: document.querySelector('body'),
     };
   }
-  const refs = getRefs();
-export function showSpinner() {
-  refs.body.classList.add('loading');
-}
-
-export function hideSpinner() {
-  window.setTimeout(function () {
-    refs.body.classList.remove('loading');
-    refs.body.classList.add('loaded');
-  }, 1500);
-}
+  
